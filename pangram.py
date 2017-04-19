fe1ca57a37dd88887d207f5c18b78ece35870d35
mrainne <mrian.tamminen@gmail.com>
2017-04-19 15:03:56 +0300

    Update pangram.py
    More elegant solution.

--- pangram.py
+++ pangram.py
@@ -2,12 +2,10 @@ def is_pangram(sentence):
 	""" Determine if all of the alphabets occur at least once in a sentence. """
 
 	# alphabets
-	alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-	for c in alphabet:
-		# alphabet must occur either in lowercase or uppercase
-		if  (c.lower() not in sentence) and (c.upper() not in sentence):
-			return False
-			break
+	alphabet = set('abcdefghijklmnopqrstuvwxyz')
 	
-	return True
+	# remove duplicate letters and non-alphabetic letters from sentence and set it lowercase letters.
+	new_sentence = set(sentence.lower())
+	
+	# if alphabet minus new_sentence returns empty set (False) the sentence is pangram.
+	return not alphabet - new_sentence
